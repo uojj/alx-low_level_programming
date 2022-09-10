add2139f47cdd25b40f96923267bfcfba1440970
Usman Jaji <uojaji@gmail.com>
2022-09-10 12:08:48 -0700

    more commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+  
+{
+  
+  // initializing strings
+  
+  char str1[] = "Programming is like building a multilingual puzzle";
+  
+  // writing to stdout
+  
+  puts(str1);
+  
+  return 0;
+  
+}
