6f83951b8f8f871852ef669e851d409ea75d6f4d
Usman Jaji <uojaji@gmail.com>
2022-09-12 14:07:56 -0700

    update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,21 +3,12 @@
 int main(void)
 {
   /* declare a variable */
-  
   char alphabet;
-  
-  for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
-    
+  for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)    
     {
-      
       /* print the value of the variable */
-      
       putchar(alphabet);
-      
     }
-    
-  putchar('\n');
-  
-  return (0);
-  
+  ptchar('\n');
+  return 0
 }
