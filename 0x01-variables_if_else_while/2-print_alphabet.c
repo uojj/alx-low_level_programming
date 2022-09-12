7decfd5ab3ba556d9b6c0383ab17387a2c035e39
Usman Jaji <uojaji@gmail.com>
2022-09-12 12:15:00 -0700

    first commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+
+int main(void)
+  
+{
+  
+  //declare a variable
+  
+  char alphabet;
+  
+
+  
+  for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+    
+    {
+      
+      // print the value of the variable
+      
+      putchar(alphabet);
+      
+
+      
+    }
+  
+
+  
+  putchar('\n');
+  
+  return (0);
+  
+}
