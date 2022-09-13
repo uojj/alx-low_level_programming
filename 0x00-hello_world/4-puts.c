aefd4ab56747fa1491ea4bdbacecb96c1d653ed0
Usman Jaji <uojaji@gmail.com>
2022-09-12 21:56:36 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,8 @@
  * 
  * Return: Always 0 (Success)
  */
-int main(void)
+int main()
 {
-  
-  puts("\"Programming is like building a multilingual puzzle"\n);
-  return (0);
+  puts("Programming is like building a multilingual puzzle\n");
+  return 0;
 }
