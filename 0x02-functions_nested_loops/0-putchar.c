541a9a37296403c33185f8e98498d4671a3251d3
Usman Jaji <uojaji@gmail.com>
2022-09-14 12:47:27 -0700

    more

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,14 @@
  */
 int main(void)
 {
-  char *sh = "_putchar";
-
-  while (*sh)
-    {
-      _putchar(*sh);
-      sh++
-    }
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('e');
+  _putchar('r');
   _putchar('\n');
   return (0);
 }
