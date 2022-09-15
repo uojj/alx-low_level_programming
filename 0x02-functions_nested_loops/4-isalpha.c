778b44e14c6aaad6cbb7047ddfbdaf04d663abb8
Usman Jaji <uojaji@gmail.com>
2022-09-14 21:16:54 -0700

    more

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,9 +4,14 @@
  * @c: single letter input
  * Return: 1 if int c is a letter (lowercase or uppercase), 0 if otherwise
  */
-int _isalpha(int c);
-if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z'))) {
-    return (1);
-}   else {
-    return (0);
+int _isalpha(int c)
+{
+  if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+    {
+      return (1);
+    }
+  else
+    {
+      return (0);
+    }
 }
