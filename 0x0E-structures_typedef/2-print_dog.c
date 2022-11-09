1f0b965fe9995df52b28540e3992075f46514ca5
Usman Jaji <uojaji@gmail.com>
2022-11-09 06:49:00 -0800

    updated commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,23 +18,14 @@ void print_dog(struct dog *d)
 	{
 	  printf("Name: (nil)\n");
 	}
-      
       printf("Age: %f\n", d->age);
-      
       if (d->owner)
-	
 	{
-	  
 	  printf("Owner: %s\n", d->owner);
-	  
 	}
-      
       else
-	
 	{
-	  
 	  printf("Owner: (nil)\n");
-	  
 	}
-      
     }
+}
